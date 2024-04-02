189365eb5f341eadb440f543c6ff8646adba46b1
orca <83993279+neoyongli@users.noreply.github.com>
2024-04-02 09:46:11 +0800

    Create simple C++ template.cpp

---
+++ simple C++ template.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+#define L(i,j,k) for(int i=(j);i<=(k);++i)
+#define R(i,j,k) for(int i=(j);i>=(k);--i)
+#define rep(i, n) L(i, 1, n)
+#define all(x) x.begin(),x.end()
+#define me(x,a) memset(x,a,sizeof(x))
+#define vi vector<int>
+#define v64 vector<i64>
+
+#include<random>
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+
+}
